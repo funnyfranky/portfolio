c95178d221d3cc4be9b43db905eb2338c30b3e24
Frank <156254944+funnyfranky@users.noreply.github.com>
2024-03-28 20:25:22 -0600

    Finished second tab. Uploading so if it goes wrong then I can revert

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,37 @@
+function openWorkout(evt, workoutName) {
+  // Declare all variables
+  var i, firsttabcontent, firsttablinks;
+
+  // Get all elements with class="firsttabcontent" and hide them
+  firsttabcontent = document.getElementsByClassName("firsttabcontent");
+  for (i = 0; i < firsttabcontent.length; i++) {
+    firsttabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="firsttabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="firsttablinks" and remove the class "active"
+  firsttablinks = document.getElementsByClassName("firsttablinks");
+  for (i = 0; i < firsttablinks.length; i++) {
+    firsttablinks[i].className = firsttablinks[i].className.replace(" active", "");
+  }
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(workoutName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
+
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -18,7 +52,5 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 
-    // // Get the first tab button and simulate a click 
-    // document.getElementsByClassName("tablinks")[0].click();
   }
   
